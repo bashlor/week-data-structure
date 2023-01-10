b9ca570e6f8fc2cdc3e824925382f4f0fe40d203
Elie Samuel <bashlor@pm.me>
2023-01-10 02:07:22 +0100

    docs: ✏️ add comments for Week object

--- src/week.ts
+++ src/week.ts
@@ -4,9 +4,32 @@ import { WeekParsable, WeekTuple } from './type/week.type';
 import { TimeslotSeries } from './timeslot-series';
 import { WEEK_SEPARATOR } from './constant/time.constant';
 
+/**
+ * Represents a week with seven days.
+ *
+ * @extends {Map<DayType, Day<unknown>>}
+ */
 export class Week extends Map<DayType, Day<unknown>> {
+
+  /**
+   * Creates a new instance of the `Week` class.
+   *
+   * @param {Week} value - The existing `Week` instance to copy.
+   */
   constructor(value: Week);
+
+  /**
+   * Creates a new instance of the `Week` class.
+   *
+   * @param {WeekParsable} value - The data to use to create a new `Week` instance.
+   */
   constructor(value: WeekParsable);
+
+  /**
+   * Creates a new instance of the `Week` class.
+   *
+   * @param {WeekParsable | Week} value - The data to use to create a new `Week` instance or an existing `Week` instance to copy.
+   */
   constructor(value: WeekParsable | Week) {
     if (value instanceof Week) {
       super(new Map(value));
@@ -21,34 +44,74 @@ export class Week extends Map<DayType, Day<unknown>> {
     super(new Map(temp));
   }
 
+  /**
+   * Returns the `Day` object for Monday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Monday.
+   */
   get monday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.monday]);
   }
 
+  /**
+   * Returns the `Day` object for Tuesday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Tuesday.
+   */
   get tuesday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.tuesday]);
   }
 
+  /**
+   * Returns the `Day` object for Wednesday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Wednesday.
+   */
   get wednesday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.wednesday]);
   }
 
+  /**
+   * Returns the `Day` object for Thursday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Thursday.
+   */
   get thursday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.thursday]);
   }
 
+  /**
+   * Returns the `Day` object for Friday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Friday.
+   */
   get friday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.friday]);
   }
 
+  /**
+   * Returns the `Day` object for Saturday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Saturday.
+   */
   get saturday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.saturday]);
   }
 
+  /**
+   * Returns the `Day` object for Sunday.
+   *
+   * @returns {Day<unknown>} The `Day` object for Sunday.
+   */
   get sunday(): Day<unknown> {
     return this.get(DayLabels[DayTypeEnum.sunday]);
   }
 
+  /**
+   * Returns the days of the week as a tuple.
+   *
+   * @returns {WeekTuple} The days of the week as a tuple.
+   */
   toTuple(): WeekTuple {
     return [
       this.get(DayLabels[DayTypeEnum.monday]),
@@ -61,6 +124,13 @@ export class Week extends Map<DayType, Day<unknown>> {
     ];
   }
 
+
+  /**
+   * Creates a new `Week` instance with all days set to the same values as the provided `Day` object.
+   *
+   * @param {Day<T>} value - The `Day` object to use to create the new `Week` instance.
+   * @returns {Week} The new `Week` instance.
+   */
   static fromDay<T>(value: Day<T>) {
     return new Week({
       monday: value.toJSON().timeslots,
@@ -73,6 +143,13 @@ export class Week extends Map<DayType, Day<unknown>> {
     });
   }
 
+  /**
+   * Returns an object containing the empty timeslots for a specific day of the week or all days of the week.
+   *
+   * @param {DayType} [day] - The day of the week to get the empty timeslots for.
+   * @param {boolean} [extendToLimit=false] - Whether to extend the empty timeslots to the time limits of the day.
+   * @returns {DayTypeEmptyTimeslots | WeekTypeEmptyTimeslots} An object containing the empty timeslots for the specified day or all days of the week.
+   */
   getEmptyTimeslots(day?: DayType,extendToLimit = false) {
     if (day) {
       return this[day].getEmptyTimeslots(extendToLimit);
@@ -88,6 +165,12 @@ export class Week extends Map<DayType, Day<unknown>> {
     };
   }
 
+  /**
+   * Creates a new `Week` instance with all days set to the same values as the provided `TimeslotSeries` object.
+   *
+   * @param {TimeslotSeries} value - The `TimeslotSeries` object to use to create the new `Week` instance.
+   * @returns {Week} The new `Week` instance.
+   */
   static fromTimeslotSeries(value: TimeslotSeries): Week {
     return new Week({
       monday: value.toJSON().timeslots,
@@ -100,12 +183,24 @@ export class Week extends Map<DayType, Day<unknown>> {
     });
   }
 
+  /**
+   * Returns a string representation of the `Week` object.
+   *
+   * @returns {string} The string representation of the `Week` object.
+   */
   toString(): string {
     return this.toTuple()
-      .map((day) => day.toString({ includeDay: false }))
+      .map((day) => day.toString({ includeDay: true }))
       .join(WEEK_SEPARATOR);
   }
 
+
+  /**
+   * Returns the `Day` object for the specified day of the week.
+   *
+   * @param {DayType} key - The day of the week to get the `Day` object for.
+   * @returns {Day<unknown>} The `Day` object for the specified day of the week.
+   */
   get(key: DayType): Day<unknown> {
     const value = super.get(key);
     if (value === null) {
@@ -118,6 +213,13 @@ export class Week extends Map<DayType, Day<unknown>> {
     return value;
   }
 
+  /**
+   * Sets the `Day` object for the specified day of the week.
+   *
+   * @param {DayType} key - The day of the week to set the `Day` object for.
+   * @param {Day<unknown>} value - The `Day` object to set.
+   * @returns {this} The `Week` object.
+   */
   set(key: DayType, value: Day<unknown>): this {
     super.set(key, value);
     return this;
